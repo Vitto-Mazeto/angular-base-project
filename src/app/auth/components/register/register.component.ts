726f3ac6f96e6950325f3b0192af4a85491fbe6c
Vitto-Mazeto <vittopicma@gmail.com>
2024-01-13 12:41:50 -0300

    feat: creating first state and reducers

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -4,12 +4,15 @@ import { Store } from '@ngrx/store';
 import { register } from '../../store/actions';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 import { RouterLink } from '@angular/router';
+import { selectIsSubmitting } from '../../store/reducers';
+import { AuthStateInterface } from '../../types/authState.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'p-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
 })
 export class RegisterComponent {
   registerForm = this.fb.group({
@@ -18,7 +21,12 @@ export class RegisterComponent {
     password: ['', Validators.required],
   });
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  isSubmitting$ = this.store.select(selectIsSubmitting);
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{ auth: AuthStateInterface }>,
+  ) {}
 
   onSubmit(): void {
     if (this.registerForm.valid) {
